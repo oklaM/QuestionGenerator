2fae1a1983d6afb4ec302aabdce78e029918ab26
oklaM <675475355@qq.com>
2019-07-24 18:16:32 +0800

    befor up new

--- QuestionGeneratorTests/questions.py
+++ QuestionGeneratorTests/questions.py
@@ -1,11 +1,11 @@
-from QuestionGenerator.questions import QuestionGenerator, Answer, Question, DEBUG
-global DEBUG = True
+from QuestionGenerator.questions import QuestionGenerator, Answer, Question
+
 q = QuestionGenerator({})
 # 咳嗽
 question_list = q.generateQuestionsbyLogicVaribales(['咳嗽'])
 # (咳嗽, 1)
 answer = Answer(question_list[0].id, [question_list[0].options[0]])
-q.determineValueofLogicVariables([answer])
+print(q.determineValueofLogicVariables([answer]))
 # # (咳嗽, 0)
 # answer = Answer(question_list[0].id, [question_list[0].options[1]])
 # q.determineValueofLogicVariables([answer])
@@ -16,7 +16,7 @@ question_list = q.generateQuestionsbyLogicVaribales(['咽喉红肿'])
 # q.determineValueofLogicVariables([answer])
 # (咽喉红肿, 0)
 answer = Answer(question_list[0].id, [question_list[0].options[1]])
-q.determineValueofLogicVariables([answer])
+print(q.determineValueofLogicVariables([answer]))
 # '胸闷', '气短'
 question_list = q.generateQuestionsbyLogicVaribales(['胸闷', '气短'])
 question_list = q.generateQuestionsbyLogicVaribales(['自主输入'])
